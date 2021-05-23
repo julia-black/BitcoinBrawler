f63e9c6360495d6d130e5e927d2528956454c8e9
julia-black <b-r-cat@ya.ru>
2021-05-23 17:32:49 +0200

    Added prices chart in stock

--- app/src/main/java/com/singlelab/bitcoinbrawler/ui/stock/StockFragment.kt
+++ app/src/main/java/com/singlelab/bitcoinbrawler/ui/stock/StockFragment.kt
@@ -1,18 +1,27 @@
 package com.singlelab.bitcoinbrawler.ui.stock
 
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
+import com.github.mikephil.charting.components.YAxis.AxisDependency
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.singlelab.bitcoinbrawler.MainActivity
 import com.singlelab.bitcoinbrawler.R
 import com.singlelab.bitcoinbrawler.databinding.FragmentStockBinding
 import com.singlelab.bitcoinbrawler.model.exception.BuyingException
 import com.singlelab.bitcoinbrawler.ui.base.BaseFragment
 import com.singlelab.bitcoinbrawler.util.roundTo
+import java.util.*
 
 class StockFragment : BaseFragment() {
 
@@ -46,13 +55,51 @@ class StockFragment : BaseFragment() {
         _binding = null
     }
 
+    private fun initChart() {
+        with(binding.chart) {
+            setBackgroundColor(Color.WHITE)
+            description.isEnabled = false
+            setDrawGridBackground(false)
+            isSelected = false
+            isDragEnabled = true
+            setScaleEnabled(true)
+            setPinchZoom(true)
+            xAxis.setDrawLabels(false)
+        }
+    }
+
+    private fun setData(values: List<Entry>) {
+        with(binding.chart) {
+            val dataSets = ArrayList<ILineDataSet>()
+            dataSets.add(
+                LineDataSet(values, getString(R.string.price_btc)).apply {
+                    axisDependency = AxisDependency.LEFT
+                    color = ColorTemplate.getHoloBlue()
+                    setCircleColor(Color.WHITE)
+                    lineWidth = 2f
+                    circleRadius = 3f
+                    fillAlpha = 65
+                    fillColor = ColorTemplate.getHoloBlue()
+                    highLightColor = ContextCompat.getColor(context, R.color.black)
+                    setDrawCircleHole(false)
+                })
+            this.data = LineData(dataSets)
+        }
+    }
+
     private fun observeViewModel() {
         (activity as MainActivity).pricesLiveData.observe(viewLifecycleOwner, {
             var prices = ""
             it.forEach { price ->
                 prices = "$prices $price"
             }
-            binding.textDashboard.text = prices
+
+            val entries = it.mapIndexed { index, value ->
+                Entry(index.toFloat(), value.toFloat())
+            }
+            binding.chart.clear()
+            initChart()
+            setData(entries)
             try {
                 showInfoAmount(parseAmount(binding.amount.text))
             } catch (e: Exception) {
